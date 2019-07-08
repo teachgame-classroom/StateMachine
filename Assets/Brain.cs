2cc579f9ef24233463e5e71eda950ae4612ea239
teachgame <teachgame@163.com>
2019-07-08 12:16:10 +0800

    Init Commit

---
+++ Assets/Brain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    public CubeController cube;
+    private StateMachine stateMachine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GetComponent<CubeController>();
+        stateMachine = new StateMachine(this, new string[] { "Idle", "Move", "Jump" });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            stateMachine.SetState(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            stateMachine.SetState(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            stateMachine.SetState(2);
+        }
+
+        stateMachine.Update();
+
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            stateMachine.SetState(2);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        stateMachine.SetState(1);
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public bool isTest;
+    private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTest)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Move(h, v);
+
+            if(Input.GetKeyDown(KeyCode.J))
+            {
+                Jump();
+            }
+        }
+
+    }
+
+    public void Move(float h, float v)
+    {
+        body.MovePosition(body.position + transform.forward * v * 10 * Time.deltaTime);
+        body.MoveRotation(Quaternion.Euler(0, h * 180 * Time.deltaTime, 0) * body.rotation);
+    }
+
+    public void Jump()
+    {
+        body.velocity += Vector3.up * 10;
+    }
+}
